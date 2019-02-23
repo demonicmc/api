2d3f6494232ec22865406d43bd4fdc81a1e5be7c
demonicmc <gangrav@gmail.com>
2019-02-23 23:37:00 +0300

    добавил DTO для User

---
+++ src/main/kotlin/com/journaler/api/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.journaler.api.repository
+
+import com.journaler.api.security.User
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository : CrudRepository<User, String> {
+    fun findOneByEmail(email: String): User?
+}
